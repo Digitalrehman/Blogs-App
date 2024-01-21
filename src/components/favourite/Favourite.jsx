be1da1d2f379aac51f964e00f4c2cc1bb50dea45
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2024-01-21 12:58:54 +0500

    Update Favourite.jsx

--- src/components/favourite/Favourite.jsx
+++ src/components/favourite/Favourite.jsx
@@ -1,13 +1,72 @@
-import React from 'react'
-import Navbar from '../SeperateNavbar/Navbar'
+import React, { useEffect, useState } from "react";
+import Navbar from "../SeperateNavbar/Navbar";
+import "./Card.css";
+import { AiFillLike } from "react-icons/ai";
+import { AiOutlineLike } from "react-icons/ai";
+import moment from "moment";
+import {
+  FacebookIcon,
+  FacebookShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
+} from "react-share";
 
 const Favourite = () => {
+  let shareUrl = "https://drehmanportfolio.netlify.app";
+  let [like, setLike] = useState(false);
+  let [count, setCount] = useState("");
+  const LikeHandler = () => {
+    setLike(!like);
+    setCount(like ? count - 1 : count + 1);
+  };
+  const [book, setBook] = useState([]);
+
+  useEffect(() => {
+    const savedBook = localStorage.getItem("bookmarkedPosts");
+    setBook(JSON.parse(savedBook));
+  }, []);
+
   return (
     <>
-    <Navbar/>
-     <h1>Favourite</h1> 
-    </>
-  )
-}
+      <Navbar />
+      <div className="body-card">
+        {book.map((product, index) => (
+          <div className="card" key={index}>
+            <h2>{product.title}</h2>
+            <p className="description">{product.description}</p>
+            <h4>Author</h4>
+            <p className="footer">{product.status}</p>
+            <div className="date">
+              <p>{moment(product.createdAt).format("DD-MM-YYYY")}</p>
+            </div>
+            <div className="icons">
+              <div className="socail-icon">
+                <span className="like">
+                  {!like ? (
+                    <AiOutlineLike onClick={LikeHandler} />
+                  ) : (
+                    <AiFillLike onClick={LikeHandler} />
+                  )}
+                </span>
 
-export default Favourite
+                <span className="count">
+                  <p>{count}</p>
+                </span>
+              </div>
+              <div className="share-icon">
+                <FacebookShareButton url={shareUrl}>
+                  <FacebookIcon size={30} round={true} className="facebook" />
+                </FacebookShareButton>
+                <WhatsappShareButton url={shareUrl} className="facebook">
+                  <WhatsappIcon size={30} round={true} />
+                </WhatsappShareButton>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Favourite;
