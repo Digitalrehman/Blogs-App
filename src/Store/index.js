30887fdfedce79c7b77254c978af07eca0181298
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2024-01-21 13:00:14 +0500

    Delete src/Store directory

--- src/Store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { bookmarkitems } from "./Slice";
-
-let Store  = configureStore({
-    reducer: {
-        bookmarkitems : bookmarkitems
-    }
-})
-export default Store;
