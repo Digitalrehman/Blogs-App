7ac961deace470104d56b21354e780481fa7cb0b
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2024-01-21 12:55:49 +0500

    Update Navbar.jsx

--- src/components/SeperateNavbar/Navbar.jsx
+++ src/components/SeperateNavbar/Navbar.jsx
@@ -1,37 +1,35 @@
-import React from 'react'
-import "./Navbar.css"
+import React from "react";
+import "./Navbar.css";
 import { FaSearch } from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
-    
       <div className="navbar">
         <div className="navbar-container">
           <ul className="navbar-nav">
-            <li className="nav-item">
+            <li className="nav-item ">
               <Link to="/Home">Home</Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item home">
               <Link to="/Favourite">Favourite</Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item home">
               <Link to="/Saved">Saved</Link>
             </li>
             <li className="nav-item">
               <Link to="/Login">Logout</Link>
             </li>
-            <li className="nav-item-search">
+            <li className="nav-item-search search">
               <input type="text" placeholder="Serach Here" />
               <FaSearch />
             </li>
           </ul>
         </div>
-       
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
